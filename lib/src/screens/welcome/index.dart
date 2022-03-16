8f49e56bc3aa4def7e4251519916b23220fe2072
Dmitriy <anakondrag@gmail.com>
2022-03-16 15:41:41 +0200

    fix welcome screen

--- lib/src/screens/welcome/index.dart
+++ lib/src/screens/welcome/index.dart
@@ -1,39 +1,38 @@
+import 'package:FitnestX/src/uikit/atom/atoms.dart';
+import 'package:FitnestX/src/uikit/style/indents.dart';
 import 'package:flutter/material.dart';
 import 'package:FitnestX/src/screens/welcome/components/LogoText.dart';
 import 'package:FitnestX/src/uikit/molecule/Buttons/BlueGradientButton.dart';
 import 'package:FitnestX/src/uikit/organism/Screen.dart';
+import 'package:go_router/go_router.dart';
 
-class WellComeScreen extends StatefulWidget {
-  const WellComeScreen({Key? key}) : super(key: key);
+class WelComeScreen extends StatefulWidget {
+  const WelComeScreen({Key? key}) : super(key: key);
 
   @override
-  State<WellComeScreen> createState() => _WellComeScreenState();
+  State<WelComeScreen> createState() => _WellComeScreenState();
 }
 
-class _WellComeScreenState extends State<WellComeScreen> {
+class _WellComeScreenState extends State<WelComeScreen> {
   @override
   Widget build(BuildContext context) {
     return Screen(
+      child: const ScreenContent(
         child: Padding(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: const [
-          Expanded(
-            child: Align(
-              alignment: Alignment.center,
-              child: LogoText(),
-            ),
-          ),
-          // Expanded(
-          //   flex: 0,
-          //   child: GradientButton(
-          //     "Get Started",
-          //     isFullWidth: true, onPressed: () {  },
-          //   ),
-          // ),
-        ],
+          padding: EdgeInsets.only(top: 40 * 4),
+          child: Center(child: LogoText()),
+        ),
       ),
-      padding: const EdgeInsets.only(bottom: 40),
-    ));
+      bottomNavigationBar: Padding(
+        padding: ThemeIndents.content,
+        child: BlueGradientButton(
+          "Get Started",
+          isFullWidth: true,
+          onPressed: () {
+            context.go("/onboarding");
+          },
+        ),
+      ),
+    );
   }
 }
