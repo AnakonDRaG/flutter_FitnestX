56eb43eb6b6fac89959ae03d704a1ba5d8d85d97
Dmitriy <anakondrag@gmail.com>
2022-03-18 21:26:14 +0200

    Fix HorizontalTextDivider

--- lib/src/uikit/molecule/Components/HorizontalTextDivider.dart
+++ lib/src/uikit/molecule/Components/HorizontalTextDivider.dart
@@ -1,3 +1,4 @@
+import 'package:FitnestX/src/uikit/style/textTheme.dart';
 import 'package:flutter/material.dart';
 
 class HorizontalTextDivider extends StatelessWidget {
@@ -11,8 +12,12 @@ class HorizontalTextDivider extends StatelessWidget {
       const Expanded(
         child: Divider(),
       ),
-      Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10), child: Text(text)),
+       Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: FontSize.small),
+          )),
       const Expanded(
         child: Divider(),
       ),
